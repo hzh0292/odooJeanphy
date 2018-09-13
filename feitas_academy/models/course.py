286c6de4c0db53e07813c011ec9f82cb50c6db8a
Jeanphy <39785270+hzh0292@users.noreply.github.com>
2018-09-14 00:09:45 +0800

    Delete course.py

--- feitas_academy/models/course.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api
-
-
-class Feitas_course(models.Model):
-    _name = 'feitas.course'
-    name = fields.Char(string="名称", required=True)
-    description = fields.Text(string="课程描述")
