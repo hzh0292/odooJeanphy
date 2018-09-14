69916e819a74d4292df8b0c1219d94f16ab4d1f5
hzh0292 <hzh0292@qq.com>
2018-09-14 14:21:10 +0800

    feitas学院练习

--- feitas_academy/models/models.py
+++ feitas_academy/models/models.py
@@ -11,7 +11,7 @@ class feitas_course(models.Model):
     type = fields.Selection([('i', '理论'),
                              ('e', '实操'),
                              ('both', '理论+实操')], default='both', string='类型')
-    total_hours = fields.Float(string='总课程',compute='_total_course_hours')
+    total_hours = fields.Float(string='总课程', compute='_total_course_hours')
     lesson_hours = fields.Float(string='理论课时')
     exercise_hours = fields.Integer(string='实操课时')
 
@@ -31,7 +31,7 @@ class feitas_course(models.Model):
     @api.onchange('exercise_hours')
     def _verify_hours_3x_or_4x(self):
         if self.exercise_hours % 3 != 0 and self.exercise_hours % 4 != 0:
-            self.update({'exercise_hours':(self.exercise_hours//3+1)*3})
+            self.update({'exercise_hours': (self.exercise_hours // 3 + 1) * 3})
             return {
                 'warning': {
                     'title': "实操课时取值错误",
@@ -44,3 +44,21 @@ class feitas_course(models.Model):
         for r in self:
             if r.exercise_hours % 3 != 0 and r.exercise_hours % 4 != 0:
                 raise exceptions.ValidationError('实操课时只能是3或4的倍数')
+
+
+class Partner(models.Model):
+    _inherit = 'res.partner'
+    course_ids = fields.Many2many('feitas.course', string='负责课程', readonly=True)
+
+    @api.constrains('course_ids')
+    def _manage_course_check(self):
+        if len(self.course_ids) > 3:
+            raise exceptions.ValidationError('同一个用户不能负责3门以上的课程')
+        sum_total_hours = sum_lesson_hours = 0
+        for r in self.course_ids:
+            sum_total_hours += r.total_hours
+            sum_lesson_hours += r.lesson_hours
+        if sum_total_hours > 200:
+            raise exceptions.ValidationError('同一个用户负责总时长不能超过200小时')
+        if sum_lesson_hours > 100:
+            raise exceptions.ValidationError('同一个用户负责总理论课时时长不能超过100小时')
