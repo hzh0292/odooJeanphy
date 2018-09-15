d47cb751aacb0a64a56b7beffa3192336be933e7
hzh0292 <hzh0292@qq.com>
2018-09-15 10:31:59 +0800

    9/15

--- sale_order_jeanphy/models/models.py
+++ sale_order_jeanphy/models/models.py
@@ -5,16 +5,16 @@ from odoo import models, fields, api
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order.line'
-    x_value = fields.Float(string='在手量', readonly=True)
-    # x_value = fields.Float(string='在手数量', compute='_compute_buffer_store')
+    # x_value = fields.Float(string='在手量', readonly=True)
+    x_value = fields.Float(string='在手量', compute='_compute_buffer_store', store=True)
 
-    # @api.depends('product_id')
-    # def _compute_buffer_store(self):
-    #     for r in self:
-    #         r.x_value = r.product_id.qty_available
-
-    @api.onchange('product_id')
-    def _qty_avaiable_store(self):
+    @api.depends('product_id')
+    def _compute_buffer_store(self):
         for r in self:
-            if r.product_id:
-                r.x_value = r.product_id.qty_available
+            r.x_value = r.product_id.qty_available
+
+    # @api.onchange('product_id')
+    # def _qty_avaiable_store(self):
+    #     for r in self:
+    #         if r.product_id:
+    #             r.x_value = r.product_id.qty_available
